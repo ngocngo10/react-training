874d56d77c984cfbc9bc7dae2f496240c52c6bf6
ngothingoc10 <ngothingocbk99@gmail.com>
2022-08-16 02:54:23 -0700

    Learn jsx and render html in react

--- examples/my-app/src/index.js
+++ examples/my-app/src/index.js
@@ -4,21 +4,35 @@ import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const x = 11;
+let text = 'Goodbye';
+if (x < 10) {
+  text = "Hello";
+}
+
+const formatName = (user) => user.firstName + ' ' + user.lastName;
+const user = {
+  firstName: 'hhhh',
+  lastName: 'dddd'
+};
+
 const myElement = (
+  <div>
+    <h1 className='heading'>{text}</h1>
+    <h3>{(x) < 10 ? "Hello" : "Goodbye"}</h3>
+    <h1>
+      Hello, {formatName(user)} !
+    </h1>
   <ul>
     <li>Apples</li>
     <li>Bananas</li>
     <li>Cherries</li>
     </ul>
+  </div>
 )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myElement);
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
