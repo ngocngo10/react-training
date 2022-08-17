b5204467f2c8f674cef987c64ca8658253b56e7e
ngothingoc10 <ngothingocbk99@gmail.com>
2022-08-16 21:09:17 -0700

    Learn components and props

---
+++ examples/myapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Feature, List } from "./Components/learn-components-props";
+import Car from "./Components/learn-class-components";
+
+function App() {
+  return (
+    <>
+      <Feature></Feature>
+      <Car></Car>
+      <List></List>
+    </>
+  );
+}
+
+export default App;
