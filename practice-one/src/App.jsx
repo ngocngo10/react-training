e8fd1aaeddd5b69488cac11a9ebf5a4c39bfb196
ngothingoc10 <ngothingocbk99@gmail.com>
2022-08-24 21:03:04 -0700

    Creating the increment and decrement app

---
+++ practice-one/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import DecrementIncrement from "./DecrementIncrement";
+
+function App() {
+  return (
+    <div className="App">
+      <DecrementIncrement></DecrementIncrement>
+    </div>
+  );
+}
+
+export default App;
