4430f5c544c23b16b6882524774497445d475874
ngothingoc10 <ngothingocbk99@gmail.com>
2022-09-07 03:24:59 -0700

    Set up editorconfig file #4

--- practice-one/src/App.jsx
+++ practice-one/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      {" "}
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -28,7 +29,8 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
-}
+  );
+}  
 
-export default App
+
+export default App;
