143168f7366fea7e10e5e321589a4c01a8c3b49c
ngothingoc10 <ngothingocbk99@gmail.com>
2022-09-09 02:30:47 -0700

    Define Logo component

---
+++ practice-one/src/components/Logo/index.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+import './index.css';
+
+const Logo = () => {
+  return (
+    <Link to="/home" className="logo">
+      thegioididong
+    </Link>
+  );
+};
+
+export default Logo;
