eaae8cadea1fc6822fe0f91eb0cbfa968d4741df
ngocngo <ngothingocbk99@gmail.com>
2022-09-14 23:59:44 +0700

    Style for search input

--- practice-one/src/pages/HomePage/index.jsx
+++ practice-one/src/pages/HomePage/index.jsx
@@ -1,19 +1,19 @@
-import "./index.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import fontawesome from "@fortawesome/fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import Container from "../../layouts/Container";
 import Search from "../../components/Search";
 import Button from "../../components/Button";
+import "./index.css";
 
 fontawesome.library.add(faPlus);
 
 const HomePage = () => {
   return (
-    <>
-      <section className="products-section">
+    <div className="home-page">
+      <section className="products-action-section">
         <Container>
-          <div className="products-header">
+          <div className="products-actions">
             <Search />
             <Button type="button" className="btn-add">
               <FontAwesomeIcon icon="fa-solid fa-plus" />
@@ -21,7 +21,7 @@ const HomePage = () => {
           </div>
         </Container>
       </section>
-    </>
+    </div>
   );
 };
 
