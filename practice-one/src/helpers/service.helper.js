ae59a86cd8205aad06d773bbf9a434e7b2f28cb5
ngocngo <ngothingocbk99@gmail.com>
2022-09-16 16:39:07 +0700

    Creating service in helper folder #10

---
+++ practice-one/src/helpers/service.helper.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+
+/**
+ *  Function uses endpoint, body (optional) and method to return the requested  result by the user
+ * @param {string} endpoint
+ * @param {string} method
+ * @param {object} body
+ * @returns
+ */
+const request = async (endpoint, method = 'GET', body) => {
+  const options = {
+    method,
+    headers: {
+      'Content-type': 'application/json',
+    },
+  };
+
+  if (method === 'POST' || method === 'PUT') {
+    options.body = JSON.stringify(body);
+  }
+
+  const res = await fetch(process.env.API_HOST + endpoint, options);
+  if (!res.ok) {
+    throw new Error(`An error has occurred: ${res.status}`);
+  }
+  const result = await res.json();
+  return result;
+};
+
+/**
+ * Uses endpoint to get data by GET method
+ * @param {string} endpoint
+ * @returns data
+ */
+const get = async endpoint => request(endpoint);
+
+/**
+ * Use endpoint to delete data by DELETE method
+ * @param {string} endpoint
+ */
+const remove = async endpoint => request(endpoint, 'DELETE');
+
+/**
+ * Use endpoint and body to send data to server by POST method
+ * @param {string} endpoint
+ * @param {object} body
+ */
+const create = async (endpoint, body) => request(endpoint, 'POST', body);
+
+/**
+ * Use endpoint and body to send data to a server to update a resource by PUT method
+ * @param {string} endpoint
+ * @param {object} body
+ */
+const update = async (endpoint, body) => request(endpoint, 'PUT', body);
+
+export { get, remove, create, update };
