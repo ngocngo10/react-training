41552e3b94da8ca83e8dd8b860b85e7edbb1527d
ngocngo <ngothingocbk99@gmail.com>
2022-09-17 15:54:31 +0700

    Creating API to get products #10

---
+++ practice-one/src/services/product.service.js
@@ -0,0 +1,15 @@
+import { get, remove, create, update } from '../helpers/service.helper';
+
+/**
+ * Call to API from the service helper to get all smartphones
+ * @returns {array} smartphone list
+ */
+const getSmartphoneList = async () => {
+  const smartphoneList = await get(`/smartphones`);
+  localStorage.setItem('smartphone-list', JSON.stringify(smartphoneList));
+  return smartphoneList;
+};
+
+const productService = { getSmartphoneList };
+
+export default productService;
